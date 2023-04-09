4fdf680e9e593d31aba8553c4250e2ca4b23aff1
Balendu Rawat <balendurawat1@gmail.com>
2023-04-09 21:31:56 +0530

    almost final

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
-function Header() {
+function Header(props) {
+
+  const {address, isConnected, connect} = props;
+
+
   return (
     <header>
       <div className = "leftH">
@@ -17,7 +21,9 @@ function Header() {
       <div className = "rightH">
 
         <div className = "headerItem">Ethereum</div>
-        <div className = "connectButton">Connect</div>
+        <div className = "connectButton" onClick={connect}>
+         {isConnected ? (address.slice(0,4) + "..."+ address.slice(38)) : "connect"}
+        </div>
 
       </div>
       
